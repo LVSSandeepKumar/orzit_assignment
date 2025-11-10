38114cd9cbbc7602489a1ab12e5a31a7c3c6e642
Lingamguntla Venkata Sai Sandeep Kumar <lvssandeepkumar12@gmail.com>
2025-11-10 14:46:08 +0530

    Create API and login API working

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,17 +1,79 @@
+import bcrypt from "bcryptjs";
+
 import prisma from "../db/prismaClient.js";
+import { generateTokenAndSetCookie } from "../utils/generateTokenAndSetCookie.js";
 
 export async function createUser(req, res, next) {
   try {
-    const data = req.body; 
+    const data = req.body;
 
-    const user = await prisma.user.create(
-        data, 
-    ); 
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({
+        message: "User already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+
+    data.password = hashedPassword;
+
+    const user = await prisma.user.create({
+      data,
+    });
+
+    user.password = undefined;
 
     res.status(201).json({
-        message: "User Created", 
-    }); 
+      data: user,
+      message: "User Created",
+    });
+  } catch (error) {
+    console.error("Error in creating user", error);
+    res.json(500).json({
+      message: error.message,
+    });
+  }
+}
 
+// Login API
+
+export async function login(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    console.log("User is", user);
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    generateTokenAndSetCookie(user.id, res);
+
+    res.status(200).json({
+      message: "Login successful",
+    });
   } catch (error) {
     console.error("Error in creating user", error);
     res.json(500).json({
@@ -22,9 +84,6 @@ export async function createUser(req, res, next) {
 
 export async function updateUser(req, res, next) {
   try {
-    res.json({
-        message: "Update API", 
-    })
   } catch (error) {
     console.error("Error in creating user", error);
     res.json(500).json({
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createUser, updateUser } from "../controllers/user.controller.js";
+import { createUser, login, updateUser } from "../controllers/user.controller.js";
+import { protectRoute } from "../utils/protectRoute.js";
 
-const router = Router(); 
+const router = Router();
 
-router.post("/create", createUser); 
+router.post("/login", login);
+
+router.post("/create", createUser);
 router.put("/update", updateUser);
 
-export default router; 
+export default router;
